a891209c9b12969bd278682de12c369f3522086f
Catherine Dawe <dawe.cm@gmail.com>
2021-02-02 21:33:37 -0500

    forgot to eslint

--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,16 @@
-const eqArrays = function (arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      return false
+      return false;
     } else if (arr1.length !== arr2.length) {
-    return false
+      return false;
+    }
+    return true;
   }
-  return true
-}
-}
+};
 
 
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -19,6 +19,6 @@ const assertEqual = function (actual, expected) {
 };
 
 // TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false); // should pass 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true); // should pass 
-assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), false); // should pass 
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false); // should pass
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // should pass
+assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), false); // should pass
