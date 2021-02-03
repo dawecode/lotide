b4fb1829ee31476c68e69c64c177f1ce73ae4ad8
Catherine Dawe <dawe.cm@gmail.com>
2021-02-02 21:47:30 -0500

    testing equal arrays

---
+++ assertArraysEquals.js
@@ -0,0 +1,22 @@
+const assertArraysEquals = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑 🛑 🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    } else if (arr1.length !== arr2.length) {
+      return false;
+    }
+    return true;
+  }
+};
+
+assertArraysEquals([1, 2, 3], [1, 2, 3, 4]);
+assertArraysEquals([1, 2, 3], [1, 2, 3]);
