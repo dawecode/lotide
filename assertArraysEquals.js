198d2b9388bbd6e541c064a514cbac9773215f8c
Catherine Dawe <dawe.cm@gmail.com>
2021-02-04 15:14:48 -0500

    new version of eqArrays

--- assertArraysEquals.js
+++ assertArraysEquals.js
@@ -1,3 +1,17 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true 
+  }
+};
+
+
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
     console.log(`✅ ✅ ✅ Assertion Passed: ${arr1} === ${arr2}`);
@@ -7,16 +21,7 @@ const assertArraysEqual = function(arr1, arr2) {
 };
 
 
-const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    } else if (arr1.length !== arr2.length) {
-      return false;
-    }
-    return true;
-  }
-};
 
-assertArraysEquals([1, 2, 3], [1, 2, 3, 4]);
-assertArraysEquals([1, 2, 3], [1, 2, 3]);
+
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);// should fail 
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // should pass 
