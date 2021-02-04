0740bc90ed1a159c786a8f75aedd979cf2262df3
Catherine Dawe <dawe.cm@gmail.com>
2021-02-04 15:37:43 -0500

    estlinted maps + brand new eqArrays

---
+++ map.js
@@ -0,0 +1,42 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+
+  }
+  return results;
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑 🛑 🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+
+//TEST
+//const results1 = map(words, word => word[1])
+//console.log(results1);
+
+assertArraysEqual(map(words, word => word[0]), ['g', 'c', 't', 'm', 't']); // should pass
+assertArraysEqual(map(words, word => word[2]), ['o', 'n', undefined, 'j', 'm']); // should pass
+assertArraysEqual(map(words, word => word[1]), ['r', 'o', 'o', 'a', 'o']); // should pass
