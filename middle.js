5761808e4ea78d123d048fbeb50664d01aa7764d
Catherine Dawe <dawe.cm@gmail.com>
2021-02-08 14:04:50 -0500

    refractoring

--- middle.js
+++ middle.js
@@ -13,28 +13,6 @@ const eqArrays = function (arr1, arr2) {
 
 
 
-/*const eqArrays = function (arr1, arr2) {
-  if (arr1.length === 0 && arr2.length ===0){
-    return true 
-  } else if (arr1.length !== arr2.length) {
-    return false;
-  }for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    } 
-    return true;
-  }
-};*/
-
-const assertArraysEqual = function (actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 🛑 🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const middle = function(arr) {
   let middArr = []
    let mid = Math.round((arr.length /2) -1)
@@ -49,11 +27,5 @@ const middle = function(arr) {
   return middArr
 }
 
-//TEST MIDDLE 
-assertArraysEqual(middle([1, 2]), [])// should pass 
-assertArraysEqual(middle([1]), [])// should pass 
-assertArraysEqual(middle([1, 2, 3]), [2])// should pass 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])// should pass 
-assertArraysEqual(middle([1, 2, 3]), [3])// should fail
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [2])// should fail
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [4, 5])// should fail 
+
+module.exports = middle; 
