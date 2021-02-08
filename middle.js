841babd61165bb8b251f2f1d70c5bf61562a401e
Catherine Dawe <dawe.cm@gmail.com>
2021-02-08 14:40:49 -0500

    removed eqArrays

--- middle.js
+++ middle.js
@@ -1,30 +1,16 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true 
-  }
-};
-
-
 
 const middle = function(arr) {
-  let middArr = []
+  let middArr = [];
    let mid = Math.round((arr.length /2) -1)
   if (arr.length <= 2) {
-    middArr = []
+    middArr = [];
   } else if (arr.length % 2 === 1) {
-    middArr.push(arr[mid])
+    middArr.push(arr[mid]);
   } else if (arr.length % 2 === 0) {
-    middArr.push(arr[mid])
-    middArr.push(arr[mid +1])
+    middArr.push(arr[mid]);
+    middArr.push(arr[mid +1]);
   }
-  return middArr
+  return middArr;
 }
 
 
