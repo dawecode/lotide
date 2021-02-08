0933cf4573ba46fe411e2c5dedcf2d5ac6744187
Catherine Dawe <dawe.cm@gmail.com>
2021-02-08 14:57:49 -0500

    new test

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,24 @@
 
 const head = require('../head');
-const assertEqual = require('../assertEqual')
-assertEqual(head([5, 6, 7]), 5); // should pass
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //should pass
-assertEqual(head([5]), 5); // should pass
-assertEqual(head([]), undefined); // should pass
-assertEqual(head([5, 6, 7]), 7); // should fail
+const assert = require('chai').assert;
+
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']),'5'); 
+  });
+
+  it("returns 'Hello' for '['Hello', 'Lighthouse', 'Labs']' ", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]),'Hello'); 
+  });
+
+  it("returns undefined for '[] ", () => {
+    assert.strictEqual(head([]), undefined ); 
+  });
+
+});
+
