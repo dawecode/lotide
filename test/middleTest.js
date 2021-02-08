1d3459fe67b91cd3d44c389a1409c8fff9cbd9f1
Catherine Dawe <dawe.cm@gmail.com>
2021-02-08 15:49:22 -0500

    new test

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,22 @@
 const middle = require('../middle')
-const assertArraysEqual = require('../assertArraysEqual')
-//TEST MIDDLE 
-assertArraysEqual(middle([1, 2]), [])// should pass 
-assertArraysEqual(middle([1]), [])// should pass 
-assertArraysEqual(middle([1, 2, 3]), [2])// should pass 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])// should pass 
-assertArraysEqual(middle([1, 2, 3]), [3])// should fail
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [2])// should fail
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [4, 5])// should fail 
+const assert = require('chai').assert;
+
+describe("#head", () => {
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns [ 3, 4,] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]);
+  });
+
+});
+ 
